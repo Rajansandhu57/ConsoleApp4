e5b6561ade8e75095f1c97159b01d8590917ffb1
visualstudio <visualstudio@122-PC10>
2019-03-13 10:55:09 -0400

    Jashan

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -4,12 +4,80 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp4
+namespace BankAccountNS
 {
-    class Program
+    //Name : Rajandeep Singh - C0730233
+    //Name : Jahanpreet Singh - C0732125
+    public class BankAccount
     {
-        static void Main(string[] args)
+        private string m_customerName;
+
+        private double m_balance;
+
+        private bool m_frozen = false;
+
+
+        private BankAccount()
         {
+
+        }
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+        public void Debit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            m_balance -= amount;
+        }
+        public void Credit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            m_balance += amount;
+        }
+        private void FreezeAccount()
+        {
+            m_frozen = true;
+        }
+        private void unfreezeAccount()
+        {
+            m_frozen = false;
+        }
+        public static void Main(string[] args)
+        {
+            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+            ba.Credit(5.77);
+            ba.Credit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
         }
     }
 }
+
